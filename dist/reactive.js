7f52dc6692675c159ec7ce726159682dde5cd833
lovelin0523 <18356201582@163.com>
2020-11-16 18:46:01 +0800

    更新

---
+++ dist/reactive.js
@@ -0,0 +1,112 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["reactive"] = factory();
+	else
+		root["reactive"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n/**\r\n * 响应式MVVM对象\r\n */\n\nvar Reactive = {\n\t//创建实例\n\tcreateApp: function createApp(data) {\n\t\treturn function () {};\n\t}\n};\n\nmodule.exports = Reactive;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWFjdGl2ZS8uL3NyYy9pbmRleC5qcz9iNjM1Il0sIm5hbWVzIjpbIlJlYWN0aXZlIiwiY3JlYXRlQXBwIiwiZGF0YSIsIm1vZHVsZSIsImV4cG9ydHMiXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7Ozs7QUFHQSxJQUFNQSxXQUFXO0FBQ2hCO0FBQ0FDLFlBQVUsbUJBQUNDLElBQUQsRUFBUTtBQUNqQixTQUFPLFlBQUksQ0FFVixDQUZEO0FBR0E7QUFOZSxDQUFqQjs7QUFTQUMsT0FBT0MsT0FBUCxHQUFpQkosUUFBakIiLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJzb3VyY2VzQ29udGVudCI6WyIndXNlIHN0cmljdCdcclxuLyoqXHJcbiAqIOWTjeW6lOW8j01WVk3lr7nosaFcclxuICovXHJcbmNvbnN0IFJlYWN0aXZlID0ge1xyXG5cdC8v5Yib5bu65a6e5L6LXHJcblx0Y3JlYXRlQXBwOihkYXRhKT0+e1xyXG5cdFx0cmV0dXJuICgpPT57XHJcblx0XHRcdFxyXG5cdFx0fVxyXG5cdH1cclxufVxyXG5cclxubW9kdWxlLmV4cG9ydHMgPSBSZWFjdGl2ZSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ })
+
+/******/ });
+});
---
+++ src/index.js
@@ -0,0 +1,189 @@
+'use strict'
+
+/**
+ * 响应式MVVM对象
+ */
+class Reactive {
+	constructor() {}
+	
+	/**
+	 * 实例方法：挂载到指定节点
+	 * @param {Object} selector
+	 */
+	mount(selector){
+		let el = document.querySelector(selector)
+		if(!el){
+			el = document.body;
+		}
+		this.$el = el;
+		this.compile()
+		return this;
+	}
+
+	/**
+	 * 编译
+	 */
+	compile(){
+		//匹配{{}}的正则
+		let reg = /\{\{(.*)\}\}/g
+		let html = this.$el.outerHTML;
+		let length = 0;
+		if(html.match(reg)){
+			length = html.match(reg).length;
+		}
+		for(var i = 0;i<length;i++){
+			let res = reg.exec(html)
+			let index = res.index;
+			let key = res[1]
+			let keys = key.split('.')
+			let value = this[keys[0]]
+			for(var j = 1;j<keys.length;j++){
+				var k = value;
+				value = k[keys[j]];
+			}
+			html = html.substring(0,index) + JSON.stringify(value) + html.substring(index+res[0].length)
+		}
+		this.$el.outerHTML = html;
+	}
+	
+	/**
+	 * 创建一个Reactive的Proxy对象
+	 * @param {Object} options
+	 */
+	static createApp(options) {
+		//初始化创建一个实例
+		let instance = new Reactive()
+		//校验参数
+		let opt = Reactive._validator(options)
+		//配置watch监听参数
+		instance.$watch = opt.watch;
+		//配置methods方法参数
+		instance.$methods = opt.methods;
+		//配置data数据参数
+		instance.$data = opt.data;
+		//将data直接挂载在实例上
+		Object.keys(instance.$data).forEach(key => {
+			instance[key] = opt.data[key]
+		})
+		//将methods直接挂载在实例上
+		Object.keys(instance.$methods).forEach(key => {
+			instance[key] = opt.methods[key]
+		})
+		//进行proxy代理
+		instance = Reactive._proxy(instance)
+		//返回实例
+		return instance
+	}
+
+	/**
+	 * 获取不需要监听的属性数组
+	 */
+	static _getUnObserveProperties(){
+		return ['$el','$watch','$methods','mount'] 
+	}
+
+	/**
+	 * 检验options参数
+	 * @param {Object} options
+	 */
+	static _validator(options) {
+		let opt = {
+			data: {},
+			watch: {},
+			methods: {}
+		}
+		if (typeof options == 'object' && options) {
+
+			if (typeof options.data == 'object' && options.data) {
+				opt.data = options.data;
+			}
+
+			if (typeof options.watch == 'object' && options.watch) {
+				opt.watch = options.watch;
+			}
+
+			if (typeof options.methods == 'object' && options.methods) {
+				opt.methods = options.methods;
+			}
+		}
+		return opt
+	}
+
+	/**
+	 * proxy代理实例
+	 * @param {Object} instance
+	 */
+	static _proxy(instance) {
+		let watcher = (parentKey) => {
+			return {
+				get: (target, key) => {
+					try {
+						var watchKey = parentKey ? parentKey + '.' + key : key;
+						if(Reactive._getUnObserveProperties().includes(key)){
+							return Reflect.get(target, key)
+						}
+						return new Proxy(target[key], watcher(watchKey))
+					} catch (e) {
+						return Reflect.get(target, key)
+					}
+				},
+				set: (target, key, value) => {
+					var watchKey = parentKey ? parentKey + '.' + key : key;
+					let oldValue = Reflect.get(target, key);
+					let oldTarget = undefined;
+					//当是数组或者对象变动时获取旧的对象或数组
+					if (Array.isArray(target)) {
+						oldTarget = [...target];
+					} else if (typeof(target) == 'object' && target) {
+						oldTarget = Object.assign({}, target);
+					}
+					if (oldValue === value) {
+						return true;
+					}
+					Reflect.set(target, key, value);
+					//watch回调
+					var keys = Reactive._parseWatchKey(watchKey);
+					keys.forEach((item, index) => {
+						if (typeof(instance.$watch) == 'object' && instance.$watch[item] && typeof(instance.$watch[item]) ==
+							'function') {
+							if (index === keys.length - 1) { //对象属性监听
+								instance.$watch[item].call(instance, value, oldValue);
+							} else if (index === keys.length - 2) { //对象监听
+								instance.$watch[item].call(instance, target, oldTarget);
+							} else { //对象祖先父级监听
+								instance.$watch[item].call(instance);
+							}
+						}
+					})
+					return true;
+				}
+			}
+		}
+
+		return new Proxy(instance, watcher());
+	}
+
+	/**
+	 * 解析$watch字段数据
+	 * @param {Object} watchKey
+	 */
+	static _parseWatchKey(watchKey) {
+		var arr = watchKey.split('.');
+		var result = [];
+		var keyFirst = '';
+		for (var i = 0; i < arr.length; i++) {
+			var key = '';
+			if (keyFirst) {
+				key = keyFirst + '.' + arr[i];
+			} else {
+				key = keyFirst + arr[i];
+			}
+			keyFirst = key;
+			result.push(key);
+		}
+		return result;
+	}
+}
+
+//module.exports = Reactive
+export default Reactive
