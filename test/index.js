476b8c2b3db04078707069645cb74a3f78aa486c
lovelin0523 <18356201582@163.com>
2020-12-04 09:43:28 +0800

    更新

--- test/index.js
+++ test/index.js
@@ -1,47 +1,75 @@
 const app = Reactive.createApp({
-	data:{
-		show:false,
-		number:10,
-		version:'1.0',
-		name:'reactive.js',
-		item:'a',
-		author:{
-			name:'凌凯',
-			age:25,
-			gender:'男',
-			graduateSchool:'安徽师范大学',
-			major:'软件工程'
+	data: {
+		show: false,
+		number: 10,
+		version: '1.0',
+		name: 'reactive.js',
+		item: 'a',
+		author: {
+			name: '凌凯',
+			age: 25,
+			gender: '男',
+			graduateSchool: '安徽师范大学',
+			major: '软件工程'
 		},
-		countries:['中国','美国','英国','法国','德国','俄国','澳大利亚','日本','意大利']
+		countries: ['中国', '美国', '英国', '法国', '德国', '俄国', '澳大利亚', '日本', '意大利']
 	},
-	template(){
-		const number = this.number + 1;
+	template() {
 		return `
-			<div>
-				${number} {{this.author}}
+			<div id="app">
+				<div>
+					<span style="margin:0 10px;" lk:for="{{this.countries}}" @click="{{this.change($event,this.item,this.index)}}">{{this.item}}</span>
+				</div>
+				<div style="margin: 10px 0;">
+					<span style="margin:0 10px;" lk:for="{{this.countries}}" lk:for-index="i" lk:for-item="el">{{this.el}}{{this.i}}</span>
+				</div>
+				<div style="margin: 10px 0;">
+					<span style="margin:0 10px;" lk:for="{{this.author}}" lk:for-item="el">{{this.el}}</span>
+				</div>
+				<div id="el" lk:if="{{this.show}}">我在安徽</div>
+				<div lk:else>我在上海</div>
+				<div style="margin-bottom: 40px;">
+					<button type="button" @click="{{this.update}}">if/else控制</button>
+				</div>
+				<div lk:show="{{this.number % 2 == 0}}">元素显示与隐藏</div>
+				<div><button type="button" @click="{{this.update2}}">show控制</button></div>
 			</div>
 		`
 	},
-	mounted(){
-		//this.book = '三国演义'
+	mounted() {
+		console.log('Reactive实例渲染后')
 	},
-	methods:{
-		change(event,item,index){
+	methods: {
+		change(event, item, index) {
 			console.log(event)
-			alert(item+'||'+index)
+			alert(item + '||' + index)
 		},
-		update(){
-			if(this.show){
+		update() {
+			if (this.show) {
 				this.show = false;
-			}else {
+			} else {
 				this.show = true;
 			}
 			this.$forceUpdate();
 		},
-		update2(){
+		update2() {
 			this.number++;
 			this.$forceUpdate();
 		}
+	},
+	beforeCreate() {
+		console.log('Reactive实例创建之前')
+	},
+	created() {
+		console.log('Reactive实例创建之后')
+	},
+	beforeUpdate() {
+		console.log('Reactive实例更新之前')
+	},
+	updated() {
+		console.log('Reactive实例更新后')
+	},
+	beforeMount() {
+		console.log('Reactive实例渲染前')
 	}
 }).mount('#app')
-
